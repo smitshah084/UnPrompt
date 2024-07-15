2f382049d22e0effb9460ebc11c837aed63d7bb3
Harsh-0-7 <harshshah60@gmail.com>
2024-07-16 01:29:12 +0530

    Added return Mock function

--- frontend/src/axios/fetchData.js
+++ frontend/src/axios/fetchData.js
@@ -1,19 +1,27 @@
 import axiosInstance from "./axiosInstance";
 
-export const fetchData = async (ENDPOINT,customHeaders) => {
+export const fetchData = async (ENDPOINT, customHeaders) => {
   try {
-    const response = await axiosInstance.get(ENDPOINT,customHeaders);
+    const response = await axiosInstance.get(ENDPOINT, customHeaders);
     return response.data;
   } catch (error) {
     console.error("Error fetching data", error);
   }
 };
 
-export const postData = async (ENDPOINT, Payload,customHeaders) => {
+export const postData = async (ENDPOINT, Payload, customHeaders) => {
   try {
-    const response = await axiosInstance.post(ENDPOINT, Payload,customHeaders);
+    const response = await axiosInstance.post(ENDPOINT, Payload, customHeaders);
     return response.data;
   } catch (error) {
     console.error("Error fetching data", error);
   }
+};
+
+export const returnMock = async (value) => {
+  return new Promise((resolve)=>{
+    setTimeout(() => {
+       resolve(value);
+    }, 2000);
+  })
 };
