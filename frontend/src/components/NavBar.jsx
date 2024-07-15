c095553a86edb8483c1c5b92d6d59809fc43638b
Harsh-0-7 <harshshah60@gmail.com>
2024-07-16 01:28:41 +0530

    Made navbar sticky

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-primary-600 p-4">
+    <nav className="bg-primary-600 p-4 sticky top-0 w-full z-10">
       <div className="max-w-8xl mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <NavLink to="/" className="text-white text-xl font-bold">
