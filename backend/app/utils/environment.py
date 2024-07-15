8fe7ad8b2895ead440177d88ffd2613678d79c32
Harsh-0-7 <harshshah60@gmail.com>
2024-07-16 01:29:36 +0530

    Added keys in Config class

--- backend/app/utils/environment.py
+++ backend/app/utils/environment.py
@@ -3,19 +3,26 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 # Load environment from disk first, then apply any defaults
-load_dotenv(find_dotenv('.env'))
+load_dotenv(find_dotenv(".env"))
 
 
 class Config:
     # Application password for superadmin functions (/auth/launch_user endpoint for first-time setup)
-    APP_PASSWORD = os.environ.get('APP_PASSWORD')
+    APP_PASSWORD = os.environ.get("APP_PASSWORD")
 
     # Settings for encryption
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
-    ALGORITHM = os.environ.get('ALGORITHM', "HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', 10_080))  # one week
+    SECRET_KEY = os.environ.get("SECRET_KEY", "secret")
+    ALGORITHM = os.environ.get("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(
+        os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 10_080)
+    )  # one week
 
     # Neo4j driver execution
-    NEO4J_URI = os.environ.get('NEO4J_URI','')
-    NEO4J_USERNAME = os.environ.get('NEO4J_USERNAME', 'neo4j')
-    NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD','')
+    NEO4J_URI = os.environ.get("NEO4J_URI", "")
+    NEO4J_USERNAME = os.environ.get("NEO4J_USERNAME", "neo4j")
+    NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD", "")
+
+    AURA_INSTANCEID = os.environ.get("AURA_INSTANCEID", "")
+    AURA_INSTANCENAME = os.environ.get("AURA_INSTANCENAME", "")
+    IMAGE_KEY = os.environ.get("IMAGE_KEY", "")
+    GEMINI_KEY = os.environ.get("GEMINI_KEY", "")
